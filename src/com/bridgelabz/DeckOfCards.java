51a11ba094d591973164cc32035c174a16ec0267
TahirMansuri <tprocks786@gmail.com>
2022-02-01 20:20:22 +0530

    ADDED: Workshop on Deck Of Cards Problem

---
+++ src/com/bridgelabz/DeckOfCards.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+import java.util.Random;
+
+public class DeckOfCards {
+
+    public static void main(String[] args) {
+        //Welcome Message for User
+        System.out.println("Welcome to Deck of Cards Problem");
+    }
+}

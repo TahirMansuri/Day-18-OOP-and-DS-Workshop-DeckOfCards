edac5678a293672eb23a2848d29751e023157329
TahirMansuri <tprocks786@gmail.com>
2022-02-01 20:46:39 +0530

    MODIFIED: Sequence of Player to Play the Game

--- src/com/bridgelabz/DeckOfCards.java
+++ src/com/bridgelabz/DeckOfCards.java
@@ -13,6 +13,7 @@ public class DeckOfCards {
 
         Player player = new Player();
         player.addPlayers();
+        player.playGame();
     }
 
     public void allotCards() {
@@ -43,13 +44,61 @@ public class DeckOfCards {
             }
         }
     }
+
+    public String getRank(int rankNo) {
+        switch(rankNo) {
+            case 0:
+                return "2";
+            case 1:
+                return "3";
+            case 2:
+                return "4";
+            case 3:
+                return "5";
+            case 4:
+                return "6";
+            case 5:
+                return "7";
+            case 6:
+                return "8";
+            case 7:
+                return "9";
+            case 8:
+                return "10";
+            case 9:
+                return "JACK";
+            case 10:
+                return "QUEEN";
+            case 11:
+                return "KING";
+            case 12:
+                return "ACE";
+            default:
+                return "";
+        }
+    }
+    public String getSuit(int suitNo) {
+        switch (suitNo) {
+            case 0:
+                return "CLUBS";
+            case 1:
+                return "DIAMONDS";
+            case 2:
+                return "HEARTS";
+            case 3:
+                return "SPADES";
+            default:
+                return "";
+        }
+    }
 }
-class Player {
+class Player extends DeckOfCards{
     Scanner sc = new Scanner(System.in);
+    int playerCount;
     Player[] players;
     public void addPlayers() {
         System.out.println("Enter the Number of Players : ");
-        int playerCount = sc.nextInt();
+        playerCount = sc.nextInt();
         if(playerCount > 4 || playerCount < 2) {
             System.out.println("Please Enter the Minimum 2 or Maximum 4 Players only.");
             addPlayers();
@@ -59,6 +108,14 @@ class Player {
             for (int i = 0; i < playerCount; i++) {
                 players[i] = new Player();
             }
+        }
+    }
+
+    public void playGame() {
+        for(int i = 0; i < playerCount; i++) {
+            players[i].allotCards();
+            System.out.println("Player "+ (i+1) +"'s CARDS : ");
+            players[i].print();
         }
     }
 }
